5bf6f675bdf3d62bf1844d1b6f1a6cc60e483f69
Rick Waldron <waldron.rick@gmail.com>
2016-11-08 12:18:34 -0500

    Match readme
    Signed-off-by: Rick Waldron <waldron.rick@gmail.com>

--- examples/pir.js
+++ examples/pir.js
@@ -9,10 +9,10 @@ const pir = require('../').use(tessel.port.A.pin[2]);
 
 pir.on('ready', pir => {
   console.log('Ready and waiting...');
-  pir.on('movement', time => {
+  pir.on('movement:start', time => {
     console.log(`Something moved! Time ${time}`);
   });
-  pir.on('stillness', time => {
+  pir.on('movement:end', time => {
     console.log(`All is still. Time ${time}`);
   });
 });
